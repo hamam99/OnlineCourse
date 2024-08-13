49bd2011d0377a6c938302a1bbd23a637d632a24
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-08-13 13:42:19 +0700

    add prettier

--- src/components/shared/Courses/index.tsx
+++ src/components/shared/Courses/index.tsx
@@ -1,26 +1,37 @@
+import classNames from 'classnames'
 import { FlatList, Text, TouchableOpacity, View } from 'react-native'
 import FastImage from 'react-native-fast-image'
 import { IconBook, IconClock, IconHourglass } from 'tabler-icons-react-native'
 
 export type ICourse = {
+  id?: number
   title?: string
-  totalChapter?: number
+  total_chapter?: number
   durations?: string
   type?: 'Free' | 'Premium'
   progress?: number
   thumb?: string
+  is_purchased?: boolean
 }
 
 type CoursesProps = {
   title?: string
   courses?: ICourse[]
+  titleClassNames?: string
 }
 const Courses = (props: CoursesProps) => {
-  const { title, courses = [] } = props
+  const { title, courses = [], titleClassNames } = props
 
   const renderItem = ({ item }: { item: ICourse }) => {
+    const { progress = 0, total_chapter = 0 } = item
+    console.log(`progress`, (progress / total_chapter) * 100)
     return (
-      <TouchableOpacity className="bg-white rounded-2xl p-3 border-2 border-[#00000029] w-fit h-fit ">
+      <TouchableOpacity
+        className="bg-white rounded-2xl p-3 border-2 border-[#00000029] w-fit h-fit "
+        style={{
+          gap: 4,
+        }}
+      >
         <FastImage
           source={{
             uri: item?.thumb,
@@ -34,7 +45,7 @@ const Courses = (props: CoursesProps) => {
           <View className="flex-row items-center gap-1">
             <IconBook size={18} />
             <Text className="font-outfit text-xs text-black">
-              {item?.totalChapter} Chapters
+              {item?.total_chapter} Chapters
             </Text>
           </View>
           <View className="flex-row items-center gap-1">
@@ -44,13 +55,33 @@ const Courses = (props: CoursesProps) => {
             </Text>
           </View>
         </View>
-        <Text className="font-outfit text-base text-violet">{item?.type}</Text>
+        {item?.is_purchased ? (
+          <View className="w-fit bg-gray-300 h-2 rounded-full">
+            <View
+              className={classNames('bg-violet h-full rounded-full')}
+              style={{
+                width: `${(progress / total_chapter) * 100}%`,
+              }}
+            />
+          </View>
+        ) : (
+          <Text className="font-outfit text-base text-violet">
+            {item?.type}
+          </Text>
+        )}
       </TouchableOpacity>
     )
   }
   return (
     <View className="gap-2 w-fit h-fit">
-      <Text className="font-outfit text-2xl text-black font-bold">{title}</Text>
+      <Text
+        className={classNames(
+          'font-outfit text-2xl text-black font-bold',
+          titleClassNames,
+        )}
+      >
+        {title}
+      </Text>
       <FlatList
         data={courses}
         renderItem={renderItem}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,17 +2,30 @@ import { ScrollView, Text, View } from 'react-native'
 import { ProfileBar, SearchBar } from '../../components/features/Home'
 import Courses, { ICourse } from '../../components/shared/Courses'
 
+const MockInprogress: ICourse[] = [
+  {
+    id: 1,
+    durations: '2h:30 min',
+    total_chapter: 15,
+    thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
+    title: 'Introduction to C++',
+    type: 'Free',
+    progress: 8,
+    is_purchased: true,
+  },
+]
+
 const MockProgramming: ICourse[] = [
   {
     durations: '2h:30 min',
-    totalChapter: 15,
+    total_chapter: 15,
     thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
     title: 'Introduction to C++',
     type: 'Free',
   },
   {
     durations: '2h:30 min',
-    totalChapter: 15,
+    total_chapter: 15,
     thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
     title: 'Introduction to B++',
     type: 'Free',
@@ -22,14 +35,14 @@ const MockProgramming: ICourse[] = [
 const MockInternetMarketing: ICourse[] = [
   {
     durations: '2h:30 min',
-    totalChapter: 15,
+    total_chapter: 15,
     thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
     title: 'Introduction to C++',
     type: 'Free',
   },
   {
     durations: '2h:30 min',
-    totalChapter: 15,
+    total_chapter: 15,
     thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
     title: 'Introduction to B++',
     type: 'Free',
@@ -46,13 +59,14 @@ const Home = () => {
         </View>
         <View className="flex-1 pl-4 bg-white mt-[-30px] rounded-l-3xl rounded-r-3xl">
           <View className="mt-[-100px]">
-            <Courses title="Programming" courses={MockProgramming} />
+            <Courses
+              title="In Progress"
+              courses={MockInprogress}
+              titleClassNames="text-white"
+            />
           </View>
           <View className="mt-3">
-            <Courses
-              title="Internet Marketing"
-              courses={MockInternetMarketing}
-            />
+            <Courses title="Internet Marketing" courses={MockProgramming} />
           </View>
           <View className="mt-3">
             <Courses
