189f61fefb5b51e041de814e3f56921c15c1b1da
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-08-13 09:54:24 +0700

    update ui

---
+++ src/components/shared/Courses/index.tsx
@@ -0,0 +1,66 @@
+import { FlatList, Text, TouchableOpacity, View } from 'react-native'
+import FastImage from 'react-native-fast-image'
+import { IconBook, IconClock, IconHourglass } from 'tabler-icons-react-native'
+
+export type ICourse = {
+  title?: string
+  totalChapter?: number
+  durations?: string
+  type?: 'Free' | 'Premium'
+  progress?: number
+  thumb?: string
+}
+
+type CoursesProps = {
+  title?: string
+  courses?: ICourse[]
+}
+const Courses = (props: CoursesProps) => {
+  const { title, courses = [] } = props
+
+  const renderItem = ({ item }: { item: ICourse }) => {
+    return (
+      <TouchableOpacity className="bg-white rounded-2xl p-3 border-2 border-[#00000029] w-fit h-fit ">
+        <FastImage
+          source={{
+            uri: item?.thumb,
+          }}
+          className="w-[246px] h-[128px] rounded-2xl"
+        />
+        <Text className="font-outfit text-xl text-black font-medium">
+          {item?.title}
+        </Text>
+        <View className="flex-row justify-between">
+          <View className="flex-row items-center gap-1">
+            <IconBook size={18} />
+            <Text className="font-outfit text-xs text-black">
+              {item?.totalChapter} Chapters
+            </Text>
+          </View>
+          <View className="flex-row items-center gap-1">
+            <IconClock size={18} />
+            <Text className="font-outfit text-xs text-black">
+              {item?.durations}
+            </Text>
+          </View>
+        </View>
+        <Text className="font-outfit text-base text-violet">{item?.type}</Text>
+      </TouchableOpacity>
+    )
+  }
+  return (
+    <View className="gap-2 w-fit h-fit">
+      <Text className="font-outfit text-2xl text-black font-bold">{title}</Text>
+      <FlatList
+        data={courses}
+        renderItem={renderItem}
+        horizontal
+        contentContainerStyle={{
+          columnGap: 20,
+        }}
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  )
+}
+export default Courses
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,68 @@
-import { Text, View } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { ProfileBar, SearchBar } from '../../components/features/Home'
+import Courses, { ICourse } from '../../components/shared/Courses'
+
+const MockProgramming: ICourse[] = [
+  {
+    durations: '2h:30 min',
+    totalChapter: 15,
+    thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
+    title: 'Introduction to C++',
+    type: 'Free',
+  },
+  {
+    durations: '2h:30 min',
+    totalChapter: 15,
+    thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
+    title: 'Introduction to B++',
+    type: 'Free',
+  },
+]
+
+const MockInternetMarketing: ICourse[] = [
+  {
+    durations: '2h:30 min',
+    totalChapter: 15,
+    thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
+    title: 'Introduction to C++',
+    type: 'Free',
+  },
+  {
+    durations: '2h:30 min',
+    totalChapter: 15,
+    thumb: 'https://i.ytimg.com/vi/2lVDktWK-pc/maxresdefault.jpg',
+    title: 'Introduction to B++',
+    type: 'Free',
+  },
+]
 
 const Home = () => {
   return (
-    <View className="flex-1 bg-white ">
-      <View className="bg-violet p-5 pb-10">
-        <ProfileBar />
-        <SearchBar />
+    <ScrollView>
+      <View className="flex-1 bg-white pb-8">
+        <View className="bg-violet p-5 pb-[150px]">
+          <ProfileBar />
+          <SearchBar />
+        </View>
+        <View className="flex-1 pl-4 bg-white mt-[-30px] rounded-l-3xl rounded-r-3xl">
+          <View className="mt-[-100px]">
+            <Courses title="Programming" courses={MockProgramming} />
+          </View>
+          <View className="mt-3">
+            <Courses
+              title="Internet Marketing"
+              courses={MockInternetMarketing}
+            />
+          </View>
+          <View className="mt-3">
+            <Courses
+              title="Internet Marketing"
+              courses={MockInternetMarketing}
+            />
+          </View>
+        </View>
       </View>
-      <Text>HOME</Text>
-    </View>
+    </ScrollView>
   )
 }
 export default Home
