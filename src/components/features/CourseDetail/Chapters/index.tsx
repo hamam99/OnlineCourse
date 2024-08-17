e7a5ba8ce22cc80be86fa035dcf583c27a008229
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-08-17 16:16:18 +0700

    refactoring

--- src/components/features/CourseDetail/Chapters/index.tsx
+++ src/components/features/CourseDetail/Chapters/index.tsx
@@ -1,12 +1,8 @@
 import { FlatList, Text, TouchableOpacity, View } from 'react-native'
 import { ICourseChapter } from '../../../../types/ICourse'
-import classNames from 'classnames'
-import {
-  IconCircleCaretRight,
-  IconCircleCheck,
-  IconLock,
-} from 'tabler-icons-react-native'
-import Colors from '../../../../utils/Colors'
+import ChapterList from '../../../shared/ChapterList'
+import useNavigation from '../../../../navigation/useNavigation'
+import { RouteName } from '../../../../navigation/RouteName'
 
 const Chapters = () => {
   const MockChapters: ICourseChapter[] = [
@@ -33,77 +29,16 @@ const Chapters = () => {
     },
   ]
 
-  const renderItem = ({
-    item,
-    index,
-  }: {
-    item: ICourseChapter
-    index: number
-  }) => {
-    return (
-      <TouchableOpacity
-        className={classNames(
-          'rounded-2xl px-4 py-2  h-[70px] flex-row items-center space-x-2 border-2 border-violet bg-violet_3 ',
-          item?.isWatched && 'border-green  bg-green_2',
-          item?.isLocked && 'border-gray_1  ',
-        )}
-        disabled={item?.isLocked}
-      >
-        {item?.isWatched ? (
-          <IconCircleCheck size={30} color={Colors.green} />
-        ) : (
-          <Text
-            className={classNames(
-              'font-outfit text-3xl  w-[45px] text-violet',
-              item?.isWatched && 'text-green',
-              item?.isLocked && 'text-gray_1',
-            )}
-          >
-            {index < 9 && '0'}
-            {index + 1}
-          </Text>
-        )}
-
-        <Text
-          className={classNames(
-            'font-outfit text-xl flex-1 text-violet',
-            item?.isWatched && 'text-green ',
-            item?.isLocked && 'text-gray_1',
-          )}
-        >
-          {item?.title}
-        </Text>
-
-        {item?.isLocked ? (
-          <IconLock
-            size={30}
-            color={item?.isLocked ? Colors.gray_1 : Colors.violet}
-          />
-        ) : (
-          <IconCircleCaretRight
-            size={30}
-            color={item?.isWatched ? Colors.green : Colors.violet}
-          />
-        )}
-      </TouchableOpacity>
-    )
-  }
+  const { navigate } = useNavigation()
   return (
     <View className="bg-white p-4 rounded-2xl">
-      <Text className="font-outfit text-black font-medium text-2xl">
-        Chapters
-      </Text>
-      <FlatList
-        data={MockChapters}
-        renderItem={renderItem}
+      <ChapterList
+        title="Chapters"
+        chapters={MockChapters}
+        onChapterClick={item => {
+          navigate(RouteName.COURSE_VIDEO)
+        }}
         scrollEnabled={false}
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={{
-          gap: 16,
-        }}
-        style={{
-          marginTop: 20,
-        }}
       />
     </View>
   )
