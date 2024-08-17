ed08fbedca05afd97f73fd58371705ff16709b9b
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-08-17 12:41:37 +0700

    add color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
       },
       colors: {
         violet: '#6857E8',
+        violet_2: '#F3F1FF',
+        violet_3: '#6857E81C',
+        green: '#0BAE1B',
+        green_2: '#5AE8571C',
+        gray_1: '#9F9F9F',
+        black_1: '#00000075',
       },
     },
   },
