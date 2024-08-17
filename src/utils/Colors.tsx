a030f992ffb79e6802fc54576dddb6f7ca74f2af
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-08-17 12:43:10 +0700

    update colors

--- src/utils/Colors.tsx
+++ src/utils/Colors.tsx
@@ -1,5 +1,11 @@
 const Colors = {
   violet: '#6857E8',
-};
+  violet_2: '#F3F1FF',
+  violet_3: '#6857E81C',
+  green: '#0BAE1B',
+  green_2: '#5AE8571C',
+  gray_1: '#9F9F9F',
+  black_1: '#00000075',
+}
 
-export default Colors;
+export default Colors
