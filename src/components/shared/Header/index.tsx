77237d2864d423290080722a9915ed886d1bff65
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-08-17 16:16:44 +0700

    handle header back button

--- src/components/shared/Header/index.tsx
+++ src/components/shared/Header/index.tsx
@@ -1,13 +1,15 @@
 import { TouchableOpacity, View } from 'react-native'
 import { IconCircleArrowLeft } from 'tabler-icons-react-native'
+import useNavigation from '../../../navigation/useNavigation'
 
 type HeaderProps = {
   title?: string
 }
 const Header = () => {
+  const goBack = useNavigation().goBack
   return (
     <View className="bg-transparent py-2">
-      <TouchableOpacity>
+      <TouchableOpacity onPress={goBack}>
         <IconCircleArrowLeft size={28} color="black" />
       </TouchableOpacity>
     </View>
