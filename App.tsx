0901c5032d5b38451f732eda0a75f633cda68187
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-08-20 08:13:38 +0700

    clean up

--- App.tsx
+++ App.tsx
@@ -6,8 +6,6 @@
  */
 
 import React from 'react'
-import type { PropsWithChildren } from 'react'
-import { StyleSheet, Text, useColorScheme, View } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import RootNavigation from './src/navigation/RootNavigation'
 import { NavigationContainer } from '@react-navigation/native'
@@ -17,44 +15,24 @@ import { Auth0Provider } from 'react-native-auth0'
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
-      <SafeAreaView
-        style={{
-          flex: 1,
-        }}
-        edges={['top', 'left', 'right', 'bottom']}
+      <Auth0Provider
+        domain={process.env.AUTH0_DOMAIN!}
+        clientId={process.env.AUTH0_CLIENT_ID!}
       >
-        <Toast />
-
-        <Auth0Provider
-          domain={process.env.AUTH0_DOMAIN!}
-          clientId={process.env.AUTH0_CLIENT_ID!}
+        <SafeAreaView
+          style={{
+            flex: 1,
+          }}
+          edges={['top', 'left', 'right', 'bottom']}
         >
+          <Toast />
           <NavigationContainer>
             <RootNavigation />
           </NavigationContainer>
-        </Auth0Provider>
-      </SafeAreaView>
+        </SafeAreaView>
+      </Auth0Provider>
     </SafeAreaProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-})
 
 export default App
