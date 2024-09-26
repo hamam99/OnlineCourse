7d1c4738f2f4f2c3ca162d5598c3affc8793ecf3
Hamam Nasrodin <hamam.nasrodin@maybank.com>
2024-09-26 09:50:21 +0700

    integrate chapter

--- src/components/features/CourseDetail/Chapters/index.tsx
+++ src/components/features/CourseDetail/Chapters/index.tsx
@@ -1,44 +1,28 @@
-import { FlatList, Text, TouchableOpacity, View } from 'react-native'
-import { ICourseChapter } from '../../../../types/ICourse'
+import { View } from 'react-native'
 import ChapterList from '../../../shared/ChapterList'
 import useNavigation from '../../../../navigation/useNavigation'
 import { RouteName } from '../../../../navigation/RouteName'
+import useSelectedCourse from '../../../../store/useSelectedCourse'
 
 const Chapters = () => {
-  const MockChapters: ICourseChapter[] = [
-    {
-      id: 1,
-      title: 'Introduction',
-      url: 'https://www.youtube.com/watch?v=MNeX4EGtR5Y',
-      isLocked: false,
-      isWatched: true,
-    },
-    {
-      id: 2,
-      title: 'Getting Started',
-      url: 'https://www.youtube.com/watch?v=sNMtjs_wQiE',
-      isLocked: false,
-      isWatched: false,
-    },
-    {
-      id: 3,
-      title: 'What is C++',
-      url: 'https://www.youtube.com/watch?v=sNMtjs_wQiE',
-      isLocked: true,
-      isWatched: false,
-    },
-  ]
+  const {
+    isPurchased,
+    selectedCourse: course,
+    setSelectedChapter,
+  } = useSelectedCourse(state => state)
 
   const { navigate } = useNavigation()
   return (
     <View className="bg-white p-4 rounded-2xl">
       <ChapterList
         title="Chapters"
-        chapters={MockChapters}
+        chapters={course?.chapters || []}
         onChapterClick={item => {
+          setSelectedChapter(item)
           navigate(RouteName.COURSE_VIDEO)
         }}
         scrollEnabled={false}
+        isPurchased={isPurchased}
       />
     </View>
   )
